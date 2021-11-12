ca3b3a6a9d2d72fee0c49a7e710264ecb6d9fe5b
gabmor38 <Gabriela.morenor@gmail.co>
2021-11-11 22:08:12 -0500

    create error and status components

--- stories/index.js
+++ stories/index.js
@@ -15,6 +15,8 @@ import Header from "components/Appointment/Header.js";
 import Empty from "components/Appointment/Empty.js";
 import Show from "components/Appointment/Show.js";
 import Confirm from "components/Appointment/Confirm.js";
+import Status from "components/Appointment/Status.js";
+import Error from "components/Appointment/Error.js";
 
 storiesOf("Button", module)
   .addParameters({
@@ -148,4 +150,7 @@ storiesOf("InterviewerList", module)
   .add("Header", () => <Header time="12pm"/>)
   .add("Empty", () => <Empty onAdd={action("onAdd")}/>)
   .add("Show", () => <Show student ="Lydia Miller-Jones" interviewers={interviewers} onEdit={action("onEdit")} onDelete={action("onDelete")}/>)
-  .add("Confirm", () => <Confirm message ="Delete the appointment?" onConfirm={action("onConfirm")} onCancel={action("onCancel")}/>);
+  .add("Confirm", () => <Confirm message ="Delete the appointment?" onConfirm={action("onConfirm")} onCancel={action("onCancel")}/>)
+  .add("Status", () => <Status message="Deleting" />)
+  .add("Error", () => <Error  message= "Could not delete appointment." onClose={action("onClose")}/>);
+  
