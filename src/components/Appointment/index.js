b2b19b08764f6a320110a6691e6f254c3cfcfa6e
gabmor38 <Gabriela.morenor@gmail.co>
2021-11-14 22:59:20 -0500

    add show student and interviewer

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -8,13 +8,16 @@ import Empty from './Empty';
 export default function Appointment(props) {
   
 console.log(props)
-
+  
   return (
     
       <article className="appointment at">
       <Header time={props.time}> </Header>
       {props.interview ? 
-      <Show student={props.interview.student}></Show>
+      <Show student={props.interview.student}
+      interviewer={props.interview.interviewer.name}
+      >
+      </Show>
       :
       <Empty></Empty>
 
