8d265dff6af72632420fd72ed1a366c258362cb8
gabmor38 <Gabriela.morenor@gmail.co>
2021-11-16 09:35:22 -0500

    changes to  DayList and DayListItem to pass value correctly

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,8 +1,48 @@
 import React from "react";
-
+import Appointment from "components/Appointment/index";
 import "components/Application.scss";
 import DayList from "./DayList";
 import { useState } from "react";
+
+const appointments = {
+  "1": {
+    id: 1,
+    time: "12pm",
+  },
+  "2": {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 3,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  "3": {
+    id: 3,
+    time: "2pm",
+  },
+  "4": {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Archie Andrews",
+      interviewer: {
+        id: 4,
+        name: "Cohana Roy",
+        avatar: "https://i.imgur.com/FK8V841.jpg",
+      }
+    }
+  },
+  "5": {
+    id: 5,
+    time: "4pm",
+  }
+};
+
 
 export default function Application(props) {
 
@@ -25,6 +65,17 @@ export default function Application(props) {
       spots: 0,
     },
   ];
+  //iterate over the appointments
+  const appointmentArr = Object.values(appointments).map((appointment) => (
+    <Appointment
+      key={appointment.id}
+      id={appointment.id}
+      time={appointment.time}
+      interview={appointment.interview}
+    />
+
+  ));
+
   return (
     <main className="layout">
       <section className="sidebar">
@@ -36,21 +87,21 @@ export default function Application(props) {
         />
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
-        <DayList
-          days={days}
-          value={day}
-          setDay={setDay}
-        />
+          <DayList
+            days={days}
+            value={day}
+            onChange={setDay}
+          />
         </nav>
         <img
           className="sidebar__lhl sidebar--centered"
           src="images/lhl.png"
           alt="Lighthouse Labs"
         />
-       
+
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {appointmentArr}
       </section>
 
     </main>
--- src/components/DayList.jsx
+++ src/components/DayList.jsx
@@ -3,15 +3,17 @@ import DayListItem from "./DayListItem";
 
 export default function DayList(props){
 
-  const dayListItems = (props.days).map((dayData) =>
+  const dayListItems = (props.days).map((day) => {
+    return(
     <DayListItem
-      key={dayData.id}
-      name={dayData.name}
-      spots={dayData.spots}
-      selected={props.name === dayData.value}
-      setDay={() => props.onChange(dayData.name)}
+      key={day.id}
+      name={day.name}
+      spots={day.spots}
+      selected={props.value === day.name}
+      setDay={() => props.onChange(day.name)}
     />
-  );
+    )
+  });
 
   return(
     <ul>
