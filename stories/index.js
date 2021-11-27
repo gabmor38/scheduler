3df65465b54e535e11280d0e7e5383d277870912
gabmor38 <Gabriela.morenor@gmail.co>
2021-11-27 17:12:29 -0500

    add of dependencies

--- stories/index.js
+++ stories/index.js
@@ -1,10 +1,7 @@
 import React, { Fragment } from 'react'
-
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
-
 import "index.scss";
-
 import Button from "components/Button";
 import DayListItem from "components/DayListItem";
 import DayList from "components/DayList";
