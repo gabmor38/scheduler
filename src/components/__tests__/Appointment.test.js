a984dbc4e32ff96081f19095a5c38aaec2d3af34
gabmor38 <Gabriela.morenor@gmail.co>
2021-12-01 21:22:08 -0500

    creation of Appointment test

---
+++ src/components/__tests__/Appointment.test.js
@@ -0,0 +1,38 @@
+/*
+  We are rendering `<Application />` down below, so we need React.createElement
+*/
+import React from "react";
+
+/*
+  We import our helper functions from the react-testing-library
+  The render function allows us to render Components
+*/
+import { render } from "@testing-library/react";
+
+/*
+  We import the component that we are testing
+*/
+import Application from "components/Application";
+import Appointment from "components/Appointment/index"
+
+/*
+  A test that renders a React Component
+*/
+it("renders without crashing", () => {
+  render(<Application />);
+});
+
+describe("Appointment", () => {
+  
+  it("renders without crashing", () => {
+    render(<Appointment />);
+  });
+
+  xit("does something it is supposed to do", () => {
+    // ...
+  });
+
+  xit("does something else it is supposed to do", () => {
+    // ...
+  });
+});
